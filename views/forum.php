97f949633d462667d5def1d5f3e7930293f667fb
Fightan <daysi.vakhaev@gmail.com>
2022-02-15 13:03:58 +0100

    Header, footer and forum updates

---
+++ views/forum.php
@@ -0,0 +1,29 @@
+
+<h1>Forum</h1>
+
+<div id="forum">
+    <div class="container">
+        <div class="row">
+            <table id="table">
+                <colgroup>
+                    <col span="1" style="width: 55%;">
+                    <col span="1" style="width: 15%;">
+                    <col span="1" style="width: 15%;">
+                    <col span="1" style="width: 15%;">
+                </colgroup>
+                <tr>
+                    <th>Sujet</th>
+                    <th>Auteur</th>
+                    <th>Réponses</th>
+                    <th>Date</th>
+                </tr>
+                <tr>
+                    <td>Entry First Line 1</td>
+                    <td>Entry First Line 2</td>
+                    <td>Entry First Line 3</td>
+                    <td>Entry First Line 4</td>
+                </tr>
+            </table> 
+        </div>
+    </div>
+</div>
--- views/header.php
+++ views/header.php
@@ -1,23 +1,26 @@
 <nav id="header">
     <div class="container-md">
         <div id="header-inline" class="row justify-content-between align-items-center">
-            <div class="col-2 d-flex justify-content-center">
+            <div id="logo" class="col-2 d-flex justify-content-start">
                 <a href="">
-                    <i class="fa-solid fa-3x fa-newspaper"></i>
+                    <span>daily</span>
+                    <span>Code</span>
                 </a>
             </div>
             <div class="menu col-8 col-md-6 d-flex justify-content-between">
-                <a href="?p=accueil">Accueil</a>
-                <a href="?p=articles">Articles</a>
-                <a href="?p=forum">Forum</a>
-                <a href="?p=contact">Contact</a>
-                <a href="?p=a_propos">À propos</a>
+                <div class="link d-flex justify-content-center align-items-center"><a href="?p=accueil">Accueil</a></div>
+                <div class="link d-flex justify-content-center align-items-center"><a href="?p=articles">Articles</a></div>
+                <div class="link d-flex justify-content-center align-items-center"><a href="?p=forum">Forum</a></div>
+                <div class="link d-flex justify-content-center align-items-center"><a href="?p=contact">Contact</a></div>
+                <div class="link d-flex justify-content-center align-items-center"><a href="?p=a_propos">À propos</a></div>
             </div>
-            <div class="col-2">
-                <a href="">
-                    <span>Login</span>
-                    <i id="login" class="fa-solid fa-lg fa-arrow-right-to-bracket"></i>
-                </a>
+            <div class="login col-2 d-flex justify-content-end">
+                <div class="link d-flex justify-content-center align-items-center">
+                    <a href="">
+                        <span>Login</span>
+                        <i id="login" class="fa-solid fa-lg fa-arrow-right-to-bracket"></i>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
